a39f55fb0d47605b932a3b8a86c1528931a442aa
nducrypto <agbonud9@gmail.com>
2022-12-01 15:10:35 +0100

    Change Password

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,6 +19,24 @@ export const verifyUser = (req, res, next) => {
   }
 };
 
+export const verifyUserId = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token)
+      return next(createError(404, "You Don't Have Token To Do This"));
+
+    const existingUser = jwt.verify(token, process.env.JWT_SECRET);
+    if (!existingUser) return next(createError(402, "token is not valid"));
+    if (existingUser.id === req.params.id || isAdmin) {
+      next();
+    } else {
+      return next(createError(404, "You're Not Authoriized"));
+    }
+  } catch (err) {
+    next(createError(400, "Failed"));
+  }
+};
+
 export const verifyAdmin = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
--- routes/user.js
+++ routes/user.js
@@ -9,12 +9,13 @@ import {
   updateUser,
   changePassword,
 } from "../controllers/user.js";
+import { verifyUserId } from "../middleware/auth.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
-router.patch("/:id", updateUser);
-router.patch("/find/:id", changePassword);
+router.patch("/:id", verifyUserId, updateUser);
+router.patch("/find/:id", verifyUserId, changePassword);
 
 export default router;
